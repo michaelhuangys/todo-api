b73bca4691e3eed3eecf452b9df25515daeefdba
huang <yhuang@mai.sfsu.edu>
2018-04-18 01:02:07 -0700

    change

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 var mongoose=require('mongoose');
 mongoose.set('debug',true);
-mongoose.connect('mongodb://localhost/todo-api');
+mongoose.connect('mongodb://michael:1122wwo@ds247699.mlab.com:47699/heroku_r777169f');
 
 mongoose.Promise=Promise;
 
